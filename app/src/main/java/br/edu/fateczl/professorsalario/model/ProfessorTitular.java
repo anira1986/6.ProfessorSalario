7f019cc63cc1808d408e453d2af87004311e3dae
ANA PAULA SILVA <ana.osilva1986@gmail.com>
2024-10-14 11:28:58 -0300

    Update ProfessorTitular.java

--- app/src/main/java/br/edu/fateczl/professorsalario/model/ProfessorTitular.java
+++ app/src/main/java/br/edu/fateczl/professorsalario/model/ProfessorTitular.java
@@ -1,3 +1,10 @@
+/**
+ *@author:<ANA PAULA DE OLIVEIRA SILVA>
+ *RA1110482123028
+ *ANA PAULA DE OLIVEIRA SILVA
+ */
+
+
 package br.edu.fateczl.professorsalario.model;
 
 public class ProfessorTitular extends Professor {
